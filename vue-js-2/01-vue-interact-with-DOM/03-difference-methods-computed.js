851b93d6e08374665a1c4641ebab4776e967040f
Keo-PC\Keo <keonakhon.kawan@outlook.com>
2017-07-05 17:33:53 +0700

    add: increase +1, decrease -1 buttons. methods and computed (with code)

---
+++ vue-js-2/01-vue-interact-with-DOM/03-difference-methods-computed.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div id="app">
+      <button v-on:click="counter++">+1</button>
+      <button v-on:click="counter--">-1</button>
+      <button v-on:click="secondCounter++">computed +1</button>
+      <p>methods: {{ counter }} || {{ output() }}</p>
+      <p>computed: {{ secondCounter }} || {{ result }}</p>
+    </div>
+    <script src="https://unpkg.com/vue"></script>
+    <script>
+      new Vue({
+        el: '#app',
+        data: {
+          counter: 0,
+          secondCounter: 0
+        },
+        methods: {
+          output: function() {
+            console.log('methods');
+            return this.counter > 5 ? 'bigger 5' : 'smaller 5';
+          }
+        },
+        computed: {
+          result: function() {
+            console.log('computed');
+            return this.secondCounter > 5 ? 'bigger 5' : 'smaller 5';
+          }
+        }
+      });
+    </script>
+  </body>
+</html>
